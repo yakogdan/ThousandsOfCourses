e872c9a2b9b60f1b75fda5c43883863f76ff200a
Bogdan <yakogdan@gmail.com>
2025-06-08 22:31:43 +0800

    Моделям, работающим с API, добавлено окончание DTO.

--- data/src/main/java/com/yakogdan/thousandsofcourses/data/network/dto/CourseDTO.kt
+++ data/src/main/java/com/yakogdan/thousandsofcourses/data/network/dto/CourseDTO.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Course(
+data class CourseDTO(
 
     @SerialName("hasLike")
     val hasLike: Boolean,
--- data/src/main/java/com/yakogdan/thousandsofcourses/data/network/dto/CoursesDTO.kt
+++ data/src/main/java/com/yakogdan/thousandsofcourses/data/network/dto/CoursesDTO.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Courses(
+data class CoursesDTO(
     @SerialName("courses")
-    val courses: List<Course>,
+    val courses: List<CourseDTO>,
 )
